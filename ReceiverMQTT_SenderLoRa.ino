dcba4f3d1af473f44edd7c177b50a1790887dc3b
MaximeGoubault <150132808+MaximeGoubault@users.noreply.github.com>
2024-02-28 10:50:59 +0100

    Update ReceiverMQTT_SenderLoRa.ino

--- ReceiverMQTT_SenderLoRa.ino
+++ ReceiverMQTT_SenderLoRa.ino
@@ -27,8 +27,10 @@ const char mqttTopic[] = "srt5/SUPERMAX"; // Topic mis à jour
 WiFiClient net;
 MQTTClient client;
 
+// Fonction appelé au démarrage du microcontrôleur 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); //Initialisation de la Communication Série débit de 9600
+  // Initalisation LoRa
   SPI.begin(SCK, MISO, MOSI, SS);
   LoRa.setPins(SS, RST, DI0);
   if (!LoRa.begin(freq)) {
@@ -39,17 +41,17 @@ void setup() {
   connectToWiFi();
   connectToMqtt();
 }
-
+// Fonction appelé en boucle permettant d'envoyer un message LoRa tous les 5s
 void loop() {
   client.loop();
-  delay(5000); // Aide à maintenir la stabilité de la connexion WiFi
+  delay(5000); 
   Serial.print("Envoi d'un message Lora... ");
   LoRa.beginPacket();
   LoRa.print("Bonjour LoRa !");
   LoRa.endPacket();
   Serial.println("terminé");
 }
-
+// FOnction de connexion au Wifi 
 void connectToWiFi() {
   Serial.print("Connexion WiFi");
   WiFi.begin(ssid, pass);
@@ -59,12 +61,12 @@ void connectToWiFi() {
   }
   Serial.println("\nWiFi connecté");
 }
-
+//Fonction d'abonnement au topic MQTT
 void connectToMqtt() {
   client.begin(mqttServer, mqttPort, net);
   client.onMessage(messageReceived);
   Serial.print("Connexion MQTT...");
-  while (!client.connect("MaximeG")) {
+  while (!client.connect("Sylvain")) {
     Serial.print(".");
     delay(1000);
   }
@@ -72,9 +74,11 @@ void connectToMqtt() {
   client.subscribe(mqttTopic); // S'abonner au topic mis à jour
 }
 
+// Fonction de reception d'un message MQTT
 void messageReceived(String &topic, String &payload) {
   Serial.print("Message MQTT reçu : ");
   Serial.println(payload);
+  // Cette fonction sert à analyser (parser) une chaîne de caractères formatée en JSON
   StaticJsonDocument<200> doc;
   DeserializationError error = deserializeJson(doc, payload);
   if (error) {
@@ -82,11 +86,13 @@ void messageReceived(String &topic, String &payload) {
     Serial.println(error.f_str());
     return;
   }
+  // On affecte les paramètres recu dans les variables
   long frq = doc["freq"]; 
   int sf = doc["sf"];
   long bw = doc["bw"];
   //Serial.println(frq);
   //Serial.println(oldfrq);
+  // Si les paramètres sont différents des précédents alors on reconfigure la liaison LoRa
   if( (frq!=oldfrq) && (sf!=oldsf) && (bw!=oldbw) ){
   LoRa.end();
   LoRa.setFrequency(frq);
